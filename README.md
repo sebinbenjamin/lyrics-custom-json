a74f8875ea7a3642e329437f1d3ff7eadec0ccad
Sebin Benjamin <sebinbenjamin@gmail.com>
2018-12-27 10:03:36 +0530

    added extra markers and copy to clipboard

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # lyrics-custom-json
 Simple UI to make convert lyrics into custom JSON format   
+
+### Guidelines for the format
+* 4 markers are presently available. They are
+  * [Intro]
+  * [Chorus]
+  * [Rap]
+  * [Bridge] 
+  
+* Line after any of the above markers should contain the text/lyrics, and no space/newline is required. 
+
+* They must be enclosed in square brackets.
+No spaces/newline must be put unnecessarily.
+
+* New verses can be started by putting an extra newline after the previous verse line.
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -11,8 +11,12 @@ export class HomePage {
   category: string[];
   program: string[];
   lyricsInput: string;
+
   parserOutput: any[];
   contents: any;
+  copyColor: {
+    background: string;
+  };
   constructor(public navCtrl: NavController) {
     this.contents = {
       lyrics: []
@@ -26,6 +30,7 @@ export class HomePage {
     this.lyricsInput = '';
     this.category = [];
     this.program = [];
+    this.copyColor = { background: '' };
     this.contents = {
       lyrics: [{
         type: 'intro',
@@ -52,15 +57,19 @@ export class HomePage {
     };
   }
   generateJSON() {
+    //JSON generation only necessary if Lyrics has been added 
     if (this.lyricsInput.length == 0)
       return;
+
+    //Get each line from input and split into array string based on newline
     let lines = this.lyricsInput.split(/\n/);
     this.parserOutput.splice(0, this.parserOutput.length);
 
+    //for each line in input, parse any markers and produce output  
     for (let i = 0; i < lines.length; i++) {
       // Line contains a non whitespace character.
       if (/\S/.test(lines[i])) {
-        if (lines[i].toUpperCase().startsWith('INTRO') && lines[i + 1] != undefined) {
+        if (lines[i].toUpperCase().startsWith('[INTRO]') && lines[i + 1] != undefined) {
           this.parserOutput.push(
             {
               type: 'intro',
@@ -70,6 +79,37 @@ export class HomePage {
           if (i < lines.length) i++;
           continue;
         }
+        else if (lines[i].toUpperCase().startsWith('[CHORUS]') && lines[i + 1] != undefined) {
+          this.parserOutput.push(
+            {
+              type: 'chorus',
+              words: lines[i + 1].trim()
+            }
+          );
+          if (i < lines.length) i++;
+          continue;
+        }
+        else if (lines[i].toUpperCase().startsWith('[RAP]') && lines[i + 1] != undefined) {
+          this.parserOutput.push(
+            {
+              type: 'rap',
+              words: lines[i + 1].trim()
+            }
+          );
+          if (i < lines.length) i++;
+          continue;
+        }
+        else if (lines[i].toUpperCase().startsWith('[BRIDGE]') && lines[i + 1] != undefined) {
+          this.parserOutput.push(
+            {
+              type: 'bridge',
+              words: lines[i + 1].trim()
+            }
+          );
+          if (i < lines.length) i++;
+          continue;
+        }
+        //if there are no markers in the line, just add it as line
         else {
           this.parserOutput.push({
             type: 'line',
@@ -77,8 +117,13 @@ export class HomePage {
           });
         }
       }
-      // if we get a newline, recognize as next para 
-      else if (lines[i] == '' && lines[i + 1] != undefined) {
+
+      // if we get a newline, recognize as nextverse if it does NOT start with any markers  
+      else if (lines[i] == '' && lines[i + 1] != undefined &&
+        !lines[i + 1].toUpperCase().startsWith('[INTRO]') &&
+        !lines[i + 1].toUpperCase().startsWith('[CHORUS]') &&
+        !lines[i + 1].toUpperCase().startsWith('[RAP]') &&
+        !lines[i + 1].toUpperCase().startsWith('[BRIDGE]')) {
         this.parserOutput.push(
           {
             type: 'nextVerse',
@@ -88,12 +133,14 @@ export class HomePage {
         if (i < lines.length) i++;
         continue;
       }
-
     }
+
+    //convert category array into object
     let categoryObj = {};
     this.category.forEach(cat => {
       categoryObj[cat] = true;
     });
+    //convert program array into object
     let programObj = {};
     this.program.forEach(prog => {
       programObj[prog] = true;
@@ -107,5 +154,21 @@ export class HomePage {
       program: programObj,
       lyrics: this.parserOutput
     };
+  }
+  copyJSON() {
+    //copies data to clipboard
+    var textField = document.createElement('textarea');
+
+    textField.innerText = JSON.stringify(this.contents);
+    document.body.appendChild(textField);
+    textField.select();
+    document.execCommand('copy');
+    textField.remove();
+
+    //changes color of copy button
+    this.copyColor.background = 'green';
+    setTimeout(() => {
+      this.copyColor.background = 'orange';
+    }, 1000);
   }
 }
